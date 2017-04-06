168e597ab276ca5dc3758f28bc8f0c833e2dff5b
JayGJianGuo <jkland927@gmail>
2017-04-06 20:54:26 +0800

    Add font-awesomt-rails to project

--- app/models/habit_list.rb
+++ app/models/habit_list.rb
@@ -1,3 +1,7 @@
 class HabitList < ApplicationRecord
   has_many :record_days
+
+  validates :title, presence: true
+  validates :description, presence: true
+  validates :habit_type, presence: true
 end
--- app/models/record_day.rb
+++ app/models/record_day.rb
@@ -3,4 +3,5 @@ class RecordDay < ApplicationRecord
 
   validates :day_num, inclusion: { in: 1..30 }
   validates :record_content, presence: true
+  validates :keyword, presence:true
 end

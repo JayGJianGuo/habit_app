eda962b78774843e281fbc825d485bf8d11d4649
JayGJianGuo <jkland927@gmail>
2017-04-12 10:10:39 +0800

    change button style

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -16,3 +16,17 @@
 //= require bootstrap/alert
 //= require bootstrap/dropdown
 //= require_tree .
+
+/**** dropdown-menu ****/
+$(document).ready(function(){
+    $(".dropdown").hover(
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideDown("fast");
+            $(this).toggleClass('open');
+        },
+        function() {
+            $('.dropdown-menu', this).not('.in .dropdown-menu').stop( true, true ).slideUp("fast");
+            $(this).toggleClass('open');
+        }
+    );
+});
